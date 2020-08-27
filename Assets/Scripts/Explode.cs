b6774df7970d7c2e970354d13685095bfac9b770
Tomoya-H <tomoya75@hotmail.de>
2020-08-27 15:18:10 +0200

    Longer Track + End
    -added rotators
    -added goal and particles

---
+++ Assets/Scripts/Explode.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explode : MonoBehaviour
+{
+    [SerializeField] private GameObject _poofParticlePrefab;
+
+    private void OnCollisionEnter2D(Collision2D collison)
+    {
+        Instantiate(_poofParticlePrefab, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
