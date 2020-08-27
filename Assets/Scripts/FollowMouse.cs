eb0cb9f12610ae3407fd514755f45c3dfe95ca47
Tomoya-H <tomoya75@hotmail.de>
2020-08-27 11:13:51 +0200

    Testing more Elements
    - added Object following Cursor for manual testing
             - rotation not working
    - added balls and platforms

--- Assets/Scripts/FollowMouse.cs
+++ Assets/Scripts/FollowMouse.cs
@@ -4,9 +4,33 @@ using UnityEngine;
 
 public class FollowMouse : MonoBehaviour
 {
+
+    [SerializeField]
+    float movSpeed = 1.0f;
+
     void Update()
     {
+        RotateAndMoveTowardsMouse();
+    }
+
+    void RotateAndMoveTowardsMouse()
+    {
+        //get mouse position and translate to world position
         Vector2 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        transform.position = new Vector2(cursorPos.x, cursorPos.y);
+
+        //objects direction towards mouse
+        Vector2 direction = new Vector2(
+            cursorPos.x - transform.position.x,
+            cursorPos.y - transform.position.y
+            );
+
+        //rotate towards mouse
+        /*
+        float angle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.back);
+        */
+
+        //move towards mouse
+        transform.position = Vector2.Lerp(transform.position, cursorPos, movSpeed);
     }
 }
